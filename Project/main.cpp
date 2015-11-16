da97bf462e9a93b1f11220b7cca2e9e8cdc97199
Higor Senna <higorr_jf@hotmail.com>
2015-11-16 16:27:28 -0200

    Alterado main
    ao invez de vetor[4] = 0, foi vetor[tamanho-1] = 0;

--- Project/main.cpp
+++ Project/main.cpp
@@ -8,63 +8,66 @@ void preencherLista(int vetor[tamanho]){
     for(int i = 0; i <tamanho ; i++){
         cin >> vetor[i];
     }
+    cout << endl;
 }
 
-void exibirVetor(int vetor[tamanho]){
-    for(int i = 0; i <tamanho; i++){
+void exibirLista(int vetor[tamanho]){
+    if (vetor[0] == -12345){
+        cout << "\n---> Preencha a lista antes de exibir! <---\n\n";
+    }else{
+        cout << "\n---> ";
+        for(int i = 0; i <tamanho; i++){
             cout << vetor[i];
+            cout << " ";
         }
-        cout << endl;
-}
-
-void esvaziarLista(int vetor[tamanho]){
-    int ordem = 0;
-    for(int posicao = 0; posicao <tamanho; posicao++){
-        vetor[posicao] = vetor[posicao + 1];
-        vetor[tamanho-(posicao+1)] = '\0';
-            cout << "----------------------------------" << endl;
-
-           exibirVetor(vetor);
+        cout << " <---\n" << endl;
     }
 }
 
+void ordenarVetor(int vetor[tamanho]){
+    int aux = 0;
+    for (int posicao = 0; posicao < tamanho; posicao ++){
+        aux = vetor[posicao + 1];
+        vetor[posicao] = aux;
+    }
+    vetor[tamanho-1] = 0;
+}
 
+void limparLista(int vetor[tamanho]){
+    exibirLista(vetor);
+    for(int posicao = 0; posicao < tamanho; posicao++){
+        ordenarVetor(vetor);
+        cout << "----------------------------------" << endl;
 
+        exibirLista(vetor);
+    }
+}
 
 int main()
 {
     int opcao = -1;
 
     int lista[tamanho];
+    lista[0] = {-12345};
     while(opcao != 0){
         cout << "Menu : " << endl;
-        cout << "1-Mostrar Lista " << endl;
-        cout << "2-Preencher Lista " << endl;
+        cout << "1-Preencher Lista " << endl;
+        cout << "2-Mostrar Lista " << endl;
         cout << "3-Limpar Lista " << endl;
         cout << "0-Sair " << endl;
         cin >> opcao;
 
           switch(opcao){
             case 1:
-            exibirVetor(lista);
-            break;
-            case 2:
             preencherLista(lista);
             break;
+            case 2:
+            exibirLista(lista);
+            break;
             case 3:
-            esvaziarLista(lista);
+            limparLista(lista);
             break;
         }
     }
-
-
-
-
-
-
-
-
-
-
     return 0;
 }
